365f6944a2fca28f5e0b7a34ebae53e471a2d7b0
desti okta <destiokta0@gmail.com>
2025-03-19 09:37:07 +0700

    First commit

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+/*console.log("halo dunia");
+//document.write("saya senang sekali")
+*/
+
+let namaAja = "Desti"; //camelCase 
+console.log(namaAja);
+console.log(typeof(namaAja));
+namaAja = 123456;
+console.log(namaAja);
+console.log(typeof(namaAja));
+
+let isFemale = false;
+console.log(typeof(isFemale));
+
+var nama1
+//const nama2
+
+
